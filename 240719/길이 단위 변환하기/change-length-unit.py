9a8aae07a886e9faa202f7897eb5ad7ccc041224
Sarang-Han <144914664+Sarang-Han@users.noreply.github.com>
2024-07-19 11:42:25 +0900

    Title: 길이 단위 변환하기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240719/길이 단위 변환하기/change-length-unit.py
@@ -0,0 +1,11 @@
+one_ft = 30.48
+one_mi = 160934
+
+feet = 9.2
+mile = 1.3
+
+ft_to_cm = feet*one_ft
+mi_to_cm = mile*one_mi
+
+print(f"{feet}ft = {ft_to_cm:.1f}cm")
+print(f"{mile}mi = {mi_to_cm:.1f}cm")
