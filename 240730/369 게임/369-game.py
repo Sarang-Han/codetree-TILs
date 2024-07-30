8a82285023407581185413493d35d40c69e932df
Sarang-Han <144914664+Sarang-Han@users.noreply.github.com>
2024-07-30 23:44:17 +0900

    Title: 369 게임, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240730/369 게임/369-game.py
+++ 240730/369 게임/369-game.py
@@ -2,7 +2,7 @@ n = int(input())
 i = 1
 
 while i <= n :
-    if list(map(int, str(i))) in [3, 6, 9]:
+    if [int(a) for a in str(i)] in [3, 6, 9]:
         print(0, end = " ")
     else:
         if i % 3 == 0 :
