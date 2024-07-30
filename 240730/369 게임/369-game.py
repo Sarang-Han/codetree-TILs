9000dd696e4d9d82e430ba02d10216b36f523d9a
Sarang-Han <144914664+Sarang-Han@users.noreply.github.com>
2024-07-30 23:47:17 +0900

    Title: 369 게임, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/369 게임/369-game.py
+++ 240730/369 게임/369-game.py
@@ -1,12 +1,18 @@
 n = int(input())
 i = 1
 
-while i <= n :
-    if [int(i) for i in str(i)] in [3, 6, 9]:
-        print(0, end = " ")
+while i <= n:
+    # 숫자를 문자열로 변환하여 각 자리를 확인
+    digits = str(i)
+    
+    # 각 자릿수에 3, 6, 9가 포함되어 있으면 0을 출력
+    if any(d in '369' for d in digits):
+        print(0, end=" ")
+    # 3의 배수인 경우 0을 출력
+    elif i % 3 == 0:
+        print(0, end=" ")
+    # 그 외에는 숫자 자체를 출력
     else:
-        if i % 3 == 0 :
-            print(0, end = " ")
-        else :
-            print(i, end = " ")
+        print(i, end=" ")
+    
     i += 1
