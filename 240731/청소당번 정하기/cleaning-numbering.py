37511e83395fc8b25d3e40107c88c41c380def4c
Sarang-Han <144914664+Sarang-Han@users.noreply.github.com>
2024-07-31 03:05:24 +0900

    Title: 청소당번 정하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240731/청소당번 정하기/cleaning-numbering.py
+++ 240731/청소당번 정하기/cleaning-numbering.py
@@ -1,12 +1,12 @@
 n = int(input())
-cnt1 = cnt2 = cnt3 = 0
+cnt2 = cnt3 = cnt12 = 0
 
-for i in range(1, n):
+for i in range(1, n+1):
     if i % 12 == 0:
-        cnt3 += 1
+        cnt12 += 1
     elif i % 3 == 0:
-        cnt2 += 1
+        cnt3 += 1
     elif i % 2 == 0:
-        cnt1 += 1
+        cnt2 += 1
 
-print(cnt1,cnt2,cnt3, end =" ")
+print(cnt2,cnt3,cnt12, end =" ")
