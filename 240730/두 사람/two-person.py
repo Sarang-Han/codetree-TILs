1dcb0e202ac0bbffda564eb164528c01cd361eea
Sarang-Han <144914664+Sarang-Han@users.noreply.github.com>
2024-07-30 20:57:24 +0900

    Title: 두 사람, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/두 사람/two-person.py
+++ 240730/두 사람/two-person.py
@@ -3,9 +3,7 @@ second, s2 = input().split()
 first = int(first)
 second = int(second)
 
-if first >= 19 and s1 == "M":
-    print(1)
-elif second >= 19 and s2 == "M":
+if (first >= 19 and s1 == "M") or (second >= 19 and s2 == "M"):
     print(1)
 else:
     print(0)
