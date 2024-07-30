c1ad56bd5717b26d2b9aeca50c29053426ff6294
Sarang-Han <144914664+Sarang-Han@users.noreply.github.com>
2024-07-30 18:58:01 +0900

    Title: 특정 조건 두 정수 비교, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
+++ 240730/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -3,9 +3,9 @@ a, b = map(int, input().split())
 if a < b :
     print(1, end = " ")
 else :
-    print(0)
+    print(0, end = " ")
 
 if a == b :
-    print(1, end = " ")
+    print(1)
 else :
     print(0)
