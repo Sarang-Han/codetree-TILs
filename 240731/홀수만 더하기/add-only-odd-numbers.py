19ab9d20d099bd9ab723241dce47cbb2e8d4d056
Sarang-Han <144914664+Sarang-Han@users.noreply.github.com>
2024-07-31 22:38:09 +0900

    Title: 홀수만 더하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/홀수만 더하기/add-only-odd-numbers.py
+++ 240731/홀수만 더하기/add-only-odd-numbers.py
@@ -1,7 +1,8 @@
 n = int(input())
 sum = 0
 
-for i in range(1, n-1):
+for i in range(n):
+    num = int(input())
     if i % 2 != 0 and i % 3 == 0 :
-        sum += i
+        sum += num
 print(sum)
