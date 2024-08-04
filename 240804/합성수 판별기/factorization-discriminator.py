d62ce2d82c75dd46ac0e63c33a9d4a0cb19425d9
Sarang-Han <144914664+Sarang-Han@users.noreply.github.com>
2024-08-04 20:49:34 +0900

    Title: 합성수 판별기, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240804/합성수 판별기/factorization-discriminator.py
+++ 240804/합성수 판별기/factorization-discriminator.py
@@ -1,8 +1,8 @@
 n = int(input())
 is_compos = False
 
-for i in (2, n):
-    if n % i == 0:
+for i in (2, n+1):
+    if n // i == 0:
         is_compos = True
 
 print("C" if is_compos == True else "N")
